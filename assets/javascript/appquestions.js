2b647f0e479fe43e135002d5588053fb0fe2b0e3
Leroy <kleroy@deloitte.com>
2017-11-24 13:57:52 -0500

    update

---
+++ assets/javascript/appquestions.js
@@ -0,0 +1,31 @@
+var quest =[
+    "How do you create a function in JavaScript?",
+    "How do you call a function named 'myFirst'?",
+    "How to write an IF statement in JavaScript?",
+    "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+    "How does a WHILE loop start?",
+    "Which sign does jQuery use as a shortcut for jQuery?",
+    "jQuery uses CSS selectors to select elements?",
+    "What is the correct jQuery code to set the background color of all p elements to red?",
+    "With jQuery, look at the following selector: $('div.intro'). What does it select?",
+    "Which jQuery method is used to switch between adding/removing one or more classes (for CSS) from selected elements?",
+    "Which jQuery method should be used to deal with name conflicts?",
+    "Which jQuery method is used to remove selected elements?",
+    "Which jQuery method is used to perform an asynchronous HTTP request?",
+]
+
+var ans =[
+    ["", "", "", ""]
+    ["run myFirst", "myFirst()", "call.myFirst"],
+    ["", "", "",],
+    ["", "", "", ""],
+    ["", "", ""],
+    ["&", "%", "$", "*"],
+    ["True", "False",],
+    
+    
+
+
+
+
+]
